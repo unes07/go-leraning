d06e98749a3dd7b7df705b903f048d6821410f31
YOUNES BOUDOUL <44946199+unes07@users.noreply.github.com>
2021-03-03 15:44:06 +0100

    Ninja level 2 exercices 3

---
+++ 006.Ninja_level_2/03.exr/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+// Create TYPED & UNTYPED constants.
+// Print the values of the constants
+
+const (
+	a int = 5
+	b     = 14.11
+)
+
+func main() {
+	fmt.Println(a, b)
+}
