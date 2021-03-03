bbfcaaca42e6dd04b89212c9bbfaf50ae0df37a7
YOUNES BOUDOUL <44946199+unes07@users.noreply.github.com>
2021-03-03 13:50:12 +0100

    string types

---
+++ 005.programmingFondamentales/003.string/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	s := "Hello playground"
+	fmt.Println(s)
+	fmt.Printf("%T\n", s)
+
+	bs := []byte(s)
+	fmt.Println(bs)
+
+	for i := 0; i < len(s); i++ {
+		fmt.Printf("%#U ", s[i])
+	}
+
+	for i, v := range s {
+		fmt.Println(i, v)
+	}
+}
