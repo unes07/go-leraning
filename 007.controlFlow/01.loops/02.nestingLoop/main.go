3c238e2e4984b5b1aa5f427f093bd561c327df72
YOUNES BOUDOUL <44946199+unes07@users.noreply.github.com>
2021-03-06 14:45:29 +0100

    loops

---
+++ 007.controlFlow/01.loops/02.nestingLoop/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	for i := 0; i <= 100; i++ {
+		fmt.Printf("The outer loop: %d\n", i)
+		for j := 0; j < 10; j++ {
+			fmt.Printf("\t\t The inner loop^! %d\n", j)
+		}
+	}
+}
