d74fa84912d31417b28f49d16635cb53c0447b8b
YOUNES BOUDOUL <44946199+unes07@users.noreply.github.com>
2021-03-06 12:58:24 +0100

    Ninja Level 2 Exr 6

---
+++ 006.Ninja_level_2/06.exr/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+)
+
+// Using iota create 4 const for the last 4 years.
+// Print the const values
+
+const (
+	a = 2021 - iota
+	b = 2021 - iota
+	c = 2021 - iota
+	d = 2021 - iota
+)
+
+func main() {
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Println(d)
+}
