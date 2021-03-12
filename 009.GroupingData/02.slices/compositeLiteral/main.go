1ac691da1fb9565fae99e366d976053a2b996bb7
YOUNES BOUDOUL <44946199+unes07@users.noreply.github.com>
2021-03-12 14:08:05 +0100

    composite literal

---
+++ 009.GroupingData/02.slices/compositeLiteral/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// x := type{values} /composit literal
+	x := []int{4, 5, 6, 7, 9, 56}
+	fmt.Println(x)
+}
+
+// a SLICE allows you to group together Values of the same type
