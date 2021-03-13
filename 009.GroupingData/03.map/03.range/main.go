2fb5e428f727472cec62e7c91d2cbe869eb20851
YOUNES BOUDOUL <44946199+unes07@users.noreply.github.com>
2021-03-13 11:54:28 +0100

    range over a map

---
+++ 009.GroupingData/03.map/03.range/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	m := map[string]int{
+		"James":           32,
+		"Miss Moneypenny": 27,
+	}
+	fmt.Println(m)
+
+	fmt.Println(m["James"])
+
+	// not recomanded
+	fmt.Println(m["Boney"])
+
+	// better way
+	v, ok := m["Boney"]
+	fmt.Println(v)
+	fmt.Println(ok)
+
+	if v, ok := m["Boney"]; ok {
+		fmt.Println("This is from if", v)
+	}
+
+	if v, ok := m["James"]; ok {
+		fmt.Println("This is from if", v)
+	}
+
+	// add new item to m
+	m["todd"] = 33
+	fmt.Println(m)
+
+	// Range
+	for i, v := range m {
+		fmt.Println(i, v)
+	}
+}
