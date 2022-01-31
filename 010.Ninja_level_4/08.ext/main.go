af352c3202ec926c3dd9fc0f8b9b719c666798ec
Unes <boudoul.younes@gmail.com>
2022-01-31 20:59:32 +0100

    ninja level 4: exercice 8

---
+++ 010.Ninja_level_4/08.ext/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+)
+
+// Create a map of type string which is a person's name and favorite song.
+// Store three recorders in the map.
+// Print out the map.
+
+func main() {
+
+	m := map[string][]string{
+		"Yassine": {"Locked out of Heaven", "when you look me in the eyes", "when I was your man"},
+		"Younes":  {"Millions years ago", "The night we meet", "Happier"},
+	}
+
+	m["Saad"] = []string{"I'm the king of the world", "Halo", "You are the reason"}
+
+	for k, v := range m {
+		fmt.Println("Name:", k)
+		for j := range v {
+			fmt.Println("\t", v[j])
+		}
+	}
+
+}
