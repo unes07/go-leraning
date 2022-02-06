a3a43bc264de4c290cdb086fea37a55dec448b78
Unes <boudoul.younes@gmail.com>
2022-02-06 13:00:18 +0100

    func: Interfaces & polymorphism

---
+++ 013.Functions/05.methods/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+type person struct {
+	first string
+	last  string
+}
+
+type secretAgent struct {
+	person
+	ltk bool
+}
+
+func main() {
+	sa1 := secretAgent{
+		person: person{
+			first: "James",
+			last:  "Bond",
+		},
+		ltk: true,
+	}
+
+	fmt.Println(sa1)
+}
