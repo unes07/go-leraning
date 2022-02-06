d2248109384018569af282738b67e5d8ab907e33
Unes <boudoul.younes@gmail.com>
2022-02-06 15:45:16 +0100

    func: return a func

--- 013.Functions/09.return_func/main.go
+++ 013.Functions/09.return_func/main.go
@@ -3,11 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	s1 := foo()
-	fmt.Println(s1)
+
+	// return the insider func of bar()
+	x := bar()
+	fmt.Printf("%T\n", x)
+
+	// execute the insider func of bar() after storing it in x variable
+	i := x()
+	fmt.Println(i)
 }
 
-func foo() string {
-	s := "from foo"
-	return s
+func bar() func() int {
+	return func() int {
+		return 14
+	}
 }
