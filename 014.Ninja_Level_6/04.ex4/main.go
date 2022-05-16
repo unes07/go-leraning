d37fedac4e4d880a8f9af6273b97aa71fe562da9
Unes <boudoul.younes@gmail.com>
2022-05-16 22:54:32 +0100

    ninja level 4: ex 4

---
+++ 014.Ninja_Level_6/04.ex4/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+type person struct {
+	first string
+	last  string
+	age   int
+}
+
+func (p person) speak() {
+	fmt.Println("Hello, my name is", p.first, p.last, "and I am", p.age)
+}
+
+func main() {
+	p1 := person{
+		first: "James",
+		last:  "Bond",
+		age:   32,
+	}
+
+	p1.speak()
+}
