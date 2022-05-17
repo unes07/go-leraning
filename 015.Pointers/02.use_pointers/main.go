203c1f2fdeb97615ebbd357237134163e697425a
Unes <boudoul.younes@gmail.com>
2022-05-18 00:59:26 +0100

    uses of pointers: step 2

--- 015.Pointers/02.use_pointers/main.go
+++ 015.Pointers/02.use_pointers/main.go
@@ -4,14 +4,22 @@ import (
 	"fmt"
 )
 
+// *note: we use pointer to pass an adress instead of a chunk of data
+// *y : dereferencr the value of y (y is a pointer)
+
 func main() {
 	x := 0
-	foo(x)
-	fmt.Println(x)
+	fmt.Println("x befor", x)
+	fmt.Println("x befor", &x)
+	foo(&x)
+	fmt.Println("x after", x)
+	fmt.Println("x after", &x)
 }
 
-func foo(y int) {
-	fmt.Println(y)
-	y = 14
-	fmt.Println(y)
+func foo(y *int) {
+	fmt.Println("y befor", y)
+	fmt.Println("y befor", *y)
+	*y = 14
+	fmt.Println("y after", y)
+	fmt.Println("y after", *y)
 }
