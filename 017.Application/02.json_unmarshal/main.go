1c4b4db26cebcc0155e02f3dfbddc04cb682a85c
Unes <boudoul.younes@gmail.com>
2022-05-21 17:59:56 +0100

    JSON: Unmarshal

--- 017.Application/02.json_unmarshal/main.go
+++ 017.Application/02.json_unmarshal/main.go
@@ -1,12 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type person struct {
+	First string `json:"First"`
+	Last  string `json:"Last"`
+	Age   int    `json:"Age"`
+}
 
 func main() {
-
 	s := `[{"First":"James","Last":"Bond","Age":32},{"First":"Miss","Last":"Moneypenny","Age":27}]`
 
 	bs := []byte(s)
 	fmt.Printf("%T\n", s)
 	fmt.Printf("%T\n", bs)
+
+	var people []person
+
+	err := json.Unmarshal(bs, &people)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(people)
+
+	for i, v := range people {
+		fmt.Println("\nPerson Number:", i+1)
+		fmt.Println(v.First, v.Last, v.Age)
+	}
 }
